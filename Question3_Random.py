2d52ab11a7f56f4fbedbaaf342e1778111cbef4b
Sarfaraz <saifhaidra13@gmail.com>
2023-07-04 19:07:08 +0530

    [Add] Branch3 File Added Successfully...

---
+++ Question3_Random.py
@@ -0,0 +1,8 @@
+import random
+
+first = random.randrange(start=0,stop=6)
+second = random.randrange(start=5,stop=10)
+third = random.randrange(start=0,stop=10,step=3)
+print(first)
+print(second)
+print(third)
